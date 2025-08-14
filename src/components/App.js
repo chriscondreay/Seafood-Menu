8ff562c3f46409347206d44889f8071269085d2b
Chris Condreay <condreaychris@gmail.com>
2025-08-14 12:01:19 -0700

    loading fish samples

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
     state = {
@@ -11,23 +13,28 @@ class App extends React.Component {
 
     addFish = (fish) => {
         // Take a copy of the existing fishes state
-        const fishes = { ...this.state.fishes };;
-
+        const fishes = { ...this.state.fishes };
         // Add new fish to fishes variable
         fishes[`fish${Date.now()}`] = fish;
-        
         // Set new fishes object to state
         this.setState({ fishes });
-    }
+    };
+
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes })
+    };
     
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
     }
--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,13 @@
+export function formatPrice(cents) {
+    return (cents / 100).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
+};
+
 export function rando(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
-}
+};
 
 export function getFunName() {
     const adjectives = [
